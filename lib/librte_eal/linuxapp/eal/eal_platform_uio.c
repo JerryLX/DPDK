7b9b97aec8e5a9e91f69e663f5a1b65f4d44b8e5
Jerry <724733136@qq.com>
2017-04-29 15:31:05 +0800

    test

--- lib/librte_eal/linuxapp/eal/eal_platform_uio.c
+++ lib/librte_eal/linuxapp/eal/eal_platform_uio.c
@@ -333,7 +333,7 @@ platform_uio_map_resource_by_index(struct rte_platform_device *dev, int res_idx,
     printf("dev name:%s, mapidx:%d\n",devname,map_idx);
     (void)platform_map_addr;
     {
-        mapaddr = mmap(0,(size_t)dev->mem_resource[res_idx].len,PROT_READ | PROT_WRITE,
+        mapaddr = mmap(0,getpagesize(),PROT_READ | PROT_WRITE,
             MAP_SHARED, fd, 0);
         printf("%p\n",mapaddr);
     }
