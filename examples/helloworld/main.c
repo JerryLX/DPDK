9a31dd3aeac6b9ecfa021c97160159df629917df
Jerry <724733136@qq.com>
2017-04-30 12:44:23 +0800

    test

--- examples/helloworld/main.c
+++ examples/helloworld/main.c
@@ -81,7 +81,7 @@ main(int argc, char **argv)
     }  
 
     p_map = (unsigned char *)mmap(0, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,fd, 0);
-    printf("%d\n",(unsigned int)p_map);
+    printf("%p\n",p_map);
     {
     	unsigned int test;
     	*(unsigned int *)(p_map+0x14) = 0;
